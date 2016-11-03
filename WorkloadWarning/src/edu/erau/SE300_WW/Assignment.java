aa568cfa47d52677a9677946bdb142ab952dddff
Elisa <hawleye@my.erau.edu>
2016-11-03 13:03:54 -0400

    addAssignment Working

--- WorkloadWarning/src/edu/erau/SE300_WW/Assignment.java
+++ WorkloadWarning/src/edu/erau/SE300_WW/Assignment.java
@@ -6,7 +6,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 /**
- * Assignment class for Canvas-like assignment calendar
+ * Assignment class for Canvas-like assignment calendar.
  * the Assignment object will store the data of an assignment including name, type, date, and course
  * @author Elisa
  */
@@ -18,11 +18,11 @@ public class Assignment {
 	public String assignmentType;
 	
 	/**
-	 * Assignment constructor stores the assignment as 1 object
-	 * @param assignment: string entry of assignment name
-	 * @param type: string entry of assignment type - Hw, Quiz, Exam
-	 * @param date: string entry of assignment date
-	 * @param course: string entry of course assignment is due in
+	 * Assignment constructor stores the assignment as one object
+	 * @param assignment: {@link string} entry of assignment name
+	 * @param type: {@link string} entry of assignment type - Hw, Quiz, Exam
+	 * @param date: {@link string} entry of assignment date
+	 * @param course: {@link string} entry of course assignment is due in
 	 * @author Elisa
 	 */
 	public Assignment(String assignment, String type, Date date, String course){
