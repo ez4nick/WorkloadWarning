9c5623abb1faa509e3e248ae01d0a3f87a8d62aa
diprea <diprea@my.erau.edu>
2016-12-05 03:17:58 -0500

    exams list

--- WorkloadWarning/src/edu/erau/SE300_WW/ExamsList.java
+++ WorkloadWarning/src/edu/erau/SE300_WW/ExamsList.java
@@ -1,61 +1,50 @@
 package edu.erau.SE300_WW;
 
 /**
- * The Exams List class displays a list of
- * exams already scheduled for students in
- * a professor's course to aid in the professor's
- * decision of an exam date.
  * @author diprea
+ * The Exams List GUI class displays a list of already scheduled exams
+ * to the professor in order to aid in their selection of an exam date.
  */
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.EtchedBorder;
-import javax.swing.BoxLayout;
-import java.awt.GridLayout;
-import javax.swing.JTable;
-import javax.swing.JLabel;
-import java.awt.Color;
-
-public class ExamsList extends JFrame {
-
-	private JPanel contentPane;
-	private JTable table;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ExamsList frame = new ExamsList();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public ExamsList() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 150);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
-		
-		table = new JTable(4,7);
-		table.setBackground(Color.WHITE);
-		contentPane.add(table);
-	}
-
+import javax.swing.JList;
+import javax.swing.SwingUtilities;
+ 
+@SuppressWarnings("serial")
+public class ExamsListGUI extends JFrame {
+    private JList<String> dateList;
+    public ExamsListGUI() {
+        //create the model and add elements
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+        listModel.addElement("Dates of already scheduled exams:");
+        listModel.addElement("10/16/2016");
+        listModel.addElement("10/17/2016");
+        listModel.addElement("10/19/2016");
+        listModel.addElement("10/20/2016");
+        listModel.addElement("10/26/2016");
+        listModel.addElement("11/24/2016");
+        listModel.addElement("11/30/2016");
+ 
+        //create the list
+        dateList = new JList<>(listModel);
+        add(dateList);
+         
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Exams List");       
+        this.setSize(250,200);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+     
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ExamsListGUI();
+            }
+        });
+    }       
 }
+
